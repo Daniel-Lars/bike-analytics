f248a39488fb696a11673fe22d7f229b8d2ecf93
Daniel-Lars <daniel2wendel@gmail.com>
2026-03-07 15:46:13 +0100

    enriched bike usage model with customer id

--- dbt_project/models/facts/fct__bike_usage.sql
+++ dbt_project/models/facts/fct__bike_usage.sql
@@ -1,6 +1,33 @@
-SELECT
-    usage_id,
-    bike_id,
-    usage_date,
-    distance_km
-FROM {{ ref('stg__bike_usage') }}
+WITH bike_usage AS (
+
+    SELECT *
+    FROM {{ ref('stg__bike_usage') }}
+)
+,
+
+contracts AS (
+
+    SELECT *
+    FROM {{ ref('stg__contracts') }}
+)
+,
+
+bike_usage_enriched AS (
+
+    SELECT
+        bike_usage.usage_id,
+        bike_usage.bike_id,
+        contracts.customer_id,
+        bike_usage.usage_date,
+        bike_usage.distance_km
+    FROM bike_usage
+    LEFT JOIN contracts
+        ON
+            bike_usage.bike_id = contracts.bike_id
+            AND bike_usage.usage_date >= contracts.contract_start_date
+            AND bike_usage.usage_date <= contracts.contract_end_date
+
+)
+
+SELECT *
+FROM bike_usage_enriched
