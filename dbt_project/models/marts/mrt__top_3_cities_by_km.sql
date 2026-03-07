2b766899e48ac8eff90e9263c37c9ed49f84aa81
Daniel-Lars <daniel2wendel@gmail.com>
2026-03-07 15:50:50 +0100

    top 3 cities by km

---
+++ dbt_project/models/marts/mrt__top_3_cities_by_km.sql
@@ -0,0 +1,28 @@
+WITH bike_usage AS (
+
+    SELECT *
+    FROM {{ ref('fct__bike_usage') }}
+)
+,
+
+customer AS (
+
+    SELECT *
+    FROM {{ ref('dim__customer') }}
+)
+,
+
+total_km_by_city AS (
+
+    SELECT
+        customer.city,
+        sum(bike_usage.distance_km) AS total_km
+    FROM bike_usage
+    INNER JOIN customer ON bike_usage.customer_id = customer.pk_customer
+    GROUP BY customer.city
+)
+
+SELECT *
+FROM total_km_by_city
+ORDER BY total_km DESC
+LIMIT 3
