83b8727c49ea6bcd6027b3359918534ca69c2e2b
Daniel-Lars <daniel2wendel@gmail.com>
2026-03-07 15:45:30 +0100

    end_of_time var, contract timeline without overlap

--- dbt_project/models/staging/contracts/stg__contracts.sql
+++ dbt_project/models/staging/contracts/stg__contracts.sql
@@ -17,5 +17,47 @@ cleaned AS (
     FROM source
 
 )
+,
+end_date_null_replaced AS (
 
-SELECT * FROM cleaned
+    SELECT
+        contract_id,
+        customer_id,
+        bike_id,
+        contract_start_date,
+        coalesce(contract_end_date, {{ var('end_of_time') }}) AS contract_end_date,
+        monthly_rate
+    FROM cleaned
+)
+,
+
+overlapping_contract_end_dates AS (
+
+    SELECT
+        *,
+        lag(contract_start_date)
+            OVER (PARTITION BY bike_id ORDER BY contract_start_date DESC)
+            AS succession_contract_start_date
+    FROM end_date_null_replaced
+
+)
+,
+
+contract_timeline_wo_overlap AS (
+
+    SELECT
+        contract_id,
+        customer_id,
+        bike_id,
+        contract_start_date,
+        CASE
+            WHEN
+                succession_contract_start_date < contract_end_date
+                THEN cast((succession_contract_start_date - interval '1 day') AS date)
+            ELSE contract_end_date
+        END AS contract_end_date,
+        monthly_rate
+    FROM overlapping_contract_end_dates
+)
+
+SELECT * FROM contract_timeline_wo_overlap
